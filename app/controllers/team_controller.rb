56c2e929e57975d39c54393224eedfb618ef506b
Sara Walker <sarawall1187@gmail.com>
2019-03-28 23:09:11 +0000

    added team get route, new_team.erb, new team form and post route

--- app/controllers/team_controller.rb
+++ app/controllers/team_controller.rb
@@ -1,3 +1,12 @@
 class TeamController < ApplicationController
 
+  get '/teams/new' do
+    erb :'/teams/new_team'
+  end
+
+  post '/teams' do
+    @team = Team.create(:name => params[:name], :sport_type => params[:sport_type])
+    session[:athlete_id] = @team.id
+    erb :'/teams/show'
+  end
 end
